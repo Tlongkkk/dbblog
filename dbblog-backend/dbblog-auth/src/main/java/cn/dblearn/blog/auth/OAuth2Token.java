2a914e17545805452a454d745cc4a56ff875755d
tlong <501195918@qq.com>
2019-07-10 11:42:04 +0800

    添加序列化ID

--- dbblog-backend/dbblog-auth/src/main/java/cn/dblearn/blog/auth/OAuth2Token.java
+++ dbblog-backend/dbblog-auth/src/main/java/cn/dblearn/blog/auth/OAuth2Token.java
@@ -12,6 +12,9 @@ import org.apache.shiro.authc.AuthenticationToken;
  * @description Shiro 认证类
  */
 public class OAuth2Token implements AuthenticationToken {
+
+    private static final long serialVersionUID = 1L;
+    
     private String token;
 
     public OAuth2Token(String token){
